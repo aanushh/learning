bc2b83ad059ebce0ea0c3b0636710033fd129b61
aanushh <anush.kumar.1998@gmail.com>
2026-04-29 12:13:24 +0530

    chore: learn debounce mechanism

---
+++ javascript/debounce.js
@@ -0,0 +1,20 @@
+/**
+ * Debounces the callback function
+ *
+ * @param {(...args: Array<unknown>) => unknown} func
+ * @param {number} wait
+ * @returns {(...args: Array<unknown>) => void}
+ */
+function debounce(func, wait) {
+  let timerId = null;
+
+  return function (...args) {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      if (typeof func === "function") {
+        func.apply(this, args);
+      }
+    }, wait);
+  };
+}
